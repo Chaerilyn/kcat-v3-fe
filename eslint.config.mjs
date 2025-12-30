6db2bb70312ff307fe523ea099533fcbbc7c743d
chaerilyn <chaerilyn@protonmail.com>
2025-12-31 00:16:57 +0100

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
-// @ts-check
+import antfu from '@antfu/eslint-config'
+// eslint.config.mjs
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  antfu(
+    {
+      // 1. General Settings
+      formatters: true,
+    },
+    // 2. Custom Overrides (Ensures all your files are linted)
+    {
+      files: ['**/*.ts', '**/*.vue', '**/*.js', '**/*.mjs'],
+      rules: {
+        // Add custom rules here if needed
+      },
+    },
+  ),
 )
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@primevue/nuxt-module', '@nuxt/fonts'],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
   css: ['@/assets/main.css'],
   vite: {
     plugins: [
